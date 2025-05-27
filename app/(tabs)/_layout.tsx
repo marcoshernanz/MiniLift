6b0fb52c85fc9b7c5187b9c164f080069f33a8f8
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-05-27 22:13:50 +0200

    improved tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import getColor from "@/lib/getColor";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
@@ -5,13 +6,45 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: getColor("primary"),
+        tabBarInactiveTintColor: getColor("mutedForeground"),
+        tabBarStyle: {
+          backgroundColor: getColor("background"),
+          borderTopColor: getColor("border"),
+          borderTopWidth: 1,
+        },
       }}
     >
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="weight" />
-      <Tabs.Screen name="activity" />
-      <Tabs.Screen name="statistics" />
-      <Tabs.Screen name="settings" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Lifts",
+        }}
+      />
+      <Tabs.Screen
+        name="weight"
+        options={{
+          tabBarLabel: "Weight",
+        }}
+      />
+      <Tabs.Screen
+        name="activity"
+        options={{
+          tabBarLabel: "Activity",
+        }}
+      />
+      <Tabs.Screen
+        name="statistics"
+        options={{
+          tabBarLabel: "Statistics",
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          tabBarLabel: "Settings",
+        }}
+      />
     </Tabs>
   );
 }
