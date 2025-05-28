ae3803da4bac1a6597ccf49f8bddb09b7f35bf71
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-05-28 20:05:20 +0200

    add background to pages

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,5 +1,13 @@
+import getColor from "@/lib/getColor";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Settings() {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: getColor("background"),
+      }}
+    ></SafeAreaView>
+  );
 }
