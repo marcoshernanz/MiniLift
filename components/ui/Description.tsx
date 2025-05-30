db9e73a49859d6d410d92c5c4046e048a0e4f661
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-05-30 19:12:58 +0200

    custom description

---
+++ components/ui/Description.tsx
@@ -0,0 +1,15 @@
+import getColor from "@/lib/getColor";
+import { StyleSheet, TextProps } from "react-native";
+import Text from "./Text";
+
+export default function Description({ style, ...props }: TextProps) {
+  return <Text style={[styles.description, style]} {...props} />;
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 14,
+    fontWeight: "400",
+    color: getColor("mutedForeground"),
+  },
+});
