4b3b306e9f28d9b06432bfa8a26d160baeea28c3
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-06-02 18:17:10 +0200

    change index color

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,5 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function IndexScreen() {
-  return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        // backgroundColor: getColor("background"),
-        backgroundColor: "#0F0",
-      }}
-    ></SafeAreaView>
-  );
+  return <SafeAreaView></SafeAreaView>;
 }
