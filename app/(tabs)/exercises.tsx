380facd17e81703fc0784870a5b35f28e4af4771
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-06-05 19:13:08 +0200

    close exercises input on click outside

--- app/(tabs)/exercises.tsx
+++ app/(tabs)/exercises.tsx
@@ -1,5 +1,29 @@
 import SafeArea from "@/components/ui/SafeArea";
+import TextInput from "@/components/ui/TextInput";
+import Title from "@/components/ui/Title";
+import getColor from "@/lib/getColor";
+import { ScrollView, StyleSheet } from "react-native";
 
 export default function ExercisesScreen() {
-  return <SafeArea></SafeArea>;
+  return (
+    <ScrollView
+      style={styles.wrapperScrollView}
+      keyboardShouldPersistTaps="handled"
+    >
+      <SafeArea>
+        <Title style={styles.title}>Exercises</Title>
+        <TextInput placeholder="Search" />
+      </SafeArea>
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  wrapperScrollView: {
+    flex: 1,
+    backgroundColor: getColor("background"),
+  },
+  title: {
+    marginBottom: 24,
+  },
+});
