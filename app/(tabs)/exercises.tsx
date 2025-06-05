1b669313a304e104336e5ad5a0c8886ee752ccff
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-06-05 20:09:29 +0200

    refactor exercises search bar

--- app/(tabs)/exercises.tsx
+++ app/(tabs)/exercises.tsx
@@ -1,25 +1,18 @@
+import ExercisesSearchBar from "@/components/exercises/ExercisesSearchBar";
 import SafeArea from "@/components/ui/SafeArea";
-import TextInput from "@/components/ui/TextInput";
 import Title from "@/components/ui/Title";
 import getColor from "@/lib/getColor";
-import { SearchIcon } from "lucide-react-native";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet } from "react-native";
 
 export default function ExercisesScreen() {
+  const [search, setSearch] = useState("");
+
   return (
     <ScrollView style={styles.scrollView} keyboardShouldPersistTaps="handled">
       <SafeArea>
         <Title style={styles.title}>Exercises</Title>
-        <View style={styles.textInputContainer}>
-          <TextInput placeholder="Search" style={styles.textInput} />
-          <View style={styles.searchIconContainer}>
-            <SearchIcon
-              color={getColor("mutedForeground")}
-              size={18}
-              style={styles.searchIcon}
-            />
-          </View>
-        </View>
+        <ExercisesSearchBar search={search} setSearch={setSearch} />
       </SafeArea>
     </ScrollView>
   );
@@ -33,18 +26,4 @@ const styles = StyleSheet.create({
   title: {
     marginBottom: 24,
   },
-  textInputContainer: {
-    position: "relative",
-  },
-  textInput: {
-    paddingLeft: 42,
-  },
-  searchIconContainer: {
-    position: "absolute",
-    height: "100%",
-    aspectRatio: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  searchIcon: {},
 });
---
+++ components/exercises/ExercisesSearchBar.tsx
@@ -0,0 +1,48 @@
+import TextInput from "@/components/ui/TextInput";
+import getColor from "@/lib/getColor";
+import { SearchIcon } from "lucide-react-native";
+import { StyleSheet, View } from "react-native";
+
+interface Props {
+  search: string;
+  setSearch: (value: string) => void;
+}
+
+export default function ExercisesSearchBar({ search, setSearch }: Props) {
+  return (
+    <View style={styles.textInputContainer}>
+      <TextInput
+        placeholder="Search"
+        style={styles.textInput}
+        value={search}
+        onChangeText={setSearch}
+      />
+      <View style={styles.searchIconContainer}>
+        <SearchIcon color={getColor("mutedForeground")} size={18} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+    backgroundColor: getColor("background"),
+  },
+  title: {
+    marginBottom: 24,
+  },
+  textInputContainer: {
+    position: "relative",
+  },
+  textInput: {
+    paddingLeft: 41,
+  },
+  searchIconContainer: {
+    position: "absolute",
+    height: "100%",
+    aspectRatio: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
