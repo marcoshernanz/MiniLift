8b6c9a5053e831436aeb102b754242c54c426f35
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-06-06 16:21:38 +0200

    use stored exercises

--- components/exercises/ExercisesList.tsx
+++ components/exercises/ExercisesList.tsx
@@ -1,5 +1,5 @@
+import { useAppContext } from "@/context/AppContext";
 import { FlatList, StyleSheet } from "react-native";
-import { v4 as uuidv4 } from "uuid";
 import ExerciseListItem from "./ExerciseListItem";
 
 interface Props {
@@ -7,80 +7,17 @@ interface Props {
 }
 
 export default function ExercisesList({ search }: Props) {
-  const baseExercises = [
-    { name: "Squat", isFavorite: false },
-    { name: "Bench Press", isFavorite: false },
-    { name: "Deadlift", isFavorite: false },
-    { name: "Overhead Press", isFavorite: false },
-    { name: "Barbell Row", isFavorite: false },
-    { name: "Pull Up", isFavorite: false },
-    { name: "Dip", isFavorite: false },
-    { name: "Lunge", isFavorite: false },
-    { name: "Calf Raise", isFavorite: false },
-    { name: "Squat", isFavorite: false },
-    { name: "Bench Press", isFavorite: false },
-    { name: "Deadlift", isFavorite: false },
-    { name: "Overhead Press", isFavorite: false },
-    { name: "Barbell Row", isFavorite: false },
-    { name: "Pull Up", isFavorite: false },
-    { name: "Dip", isFavorite: false },
-    { name: "Lunge", isFavorite: false },
-    { name: "Calf Raise", isFavorite: false },
-    { name: "Squat", isFavorite: false },
-    { name: "Bench Press", isFavorite: false },
-    { name: "Deadlift", isFavorite: false },
-    { name: "Overhead Press", isFavorite: false },
-    { name: "Barbell Row", isFavorite: false },
-    { name: "Pull Up", isFavorite: false },
-    { name: "Dip", isFavorite: false },
-    { name: "Lunge", isFavorite: false },
-    { name: "Calf Raise", isFavorite: false },
-    { name: "Squat", isFavorite: false },
-    { name: "Bench Press", isFavorite: false },
-    { name: "Deadlift", isFavorite: false },
-    { name: "Overhead Press", isFavorite: false },
-    { name: "Barbell Row", isFavorite: false },
-    { name: "Pull Up", isFavorite: false },
-    { name: "Dip", isFavorite: false },
-    { name: "Lunge", isFavorite: false },
-    { name: "Calf Raise", isFavorite: false },
-    { name: "Squat", isFavorite: false },
-    { name: "Bench Press", isFavorite: false },
-    { name: "Deadlift", isFavorite: false },
-    { name: "Overhead Press", isFavorite: false },
-    { name: "Barbell Row", isFavorite: false },
-    { name: "Pull Up", isFavorite: false },
-    { name: "Dip", isFavorite: false },
-    { name: "Lunge", isFavorite: false },
-    { name: "Calf Raise", isFavorite: false },
-    { name: "Squat", isFavorite: false },
-    { name: "Bench Press", isFavorite: false },
-    { name: "Deadlift", isFavorite: false },
-    { name: "Overhead Press", isFavorite: false },
-    { name: "Barbell Row", isFavorite: false },
-    { name: "Pull Up", isFavorite: false },
-    { name: "Dip", isFavorite: false },
-    { name: "Lunge", isFavorite: false },
-    { name: "Calf Raise", isFavorite: false },
-    { name: "Squat", isFavorite: false },
-    { name: "Bench Press", isFavorite: false },
-    { name: "Deadlift", isFavorite: false },
-    { name: "Overhead Press", isFavorite: false },
-    { name: "Barbell Row", isFavorite: false },
-    { name: "Pull Up", isFavorite: false },
-    { name: "Dip", isFavorite: false },
-    { name: "Lunge", isFavorite: false },
-    { name: "Calf Raise", isFavorite: false },
-  ];
+  const {
+    appData: { exercises },
+  } = useAppContext();
 
-  const exercises = baseExercises.map((exercise) => ({
-    id: uuidv4(),
+  const formattedExercises = Object.values(exercises).filter((exercise) => ({
     ...exercise,
   }));
 
   return (
     <FlatList
-      data={exercises}
+      data={formattedExercises}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <ExerciseListItem item={item} />}
       contentContainerStyle={{ paddingBottom: 16 }}
