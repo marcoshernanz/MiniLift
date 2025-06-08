ab1d556015a7cdfc3027b872210e55096ffe0a49
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-06-08 10:28:27 +0200

    remove notification in settings

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,21 +1,5 @@
-import Button from "@/components/ui/Button";
 import SafeArea from "@/components/ui/SafeArea";
-import { Toast } from "@/components/ui/Toast";
 
 export default function SettingsScreen() {
-  return (
-    <SafeArea>
-      <Button
-        containerStyle={{ marginTop: 200 }}
-        onPress={() =>
-          Toast.show({
-            text: "Exercise added successfully",
-            variant: "success",
-          })
-        }
-      >
-        Show Toast
-      </Button>
-    </SafeArea>
-  );
+  return <SafeArea></SafeArea>;
 }
