83fc22a7f0032292b141719f7994150efee5107c
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-06-09 20:11:03 +0200

    activity item

---
+++ components/activity/ActivityItem.tsx
@@ -0,0 +1,62 @@
+import getColor from "@/lib/getColor";
+import { ActivityEntry } from "@/lib/hooks/useActivity";
+import { format } from "date-fns";
+import { Dimensions, ScrollView, StyleSheet, View } from "react-native";
+import Description from "../ui/Description";
+import Text from "../ui/Text";
+import ActivityLogItem from "./ActivityLogItem";
+
+interface Props {
+  item: ActivityEntry;
+}
+
+export default function ActivityItem({ item }: Props) {
+  const { logs, date } = item;
+
+  return (
+    <View
+      style={{
+        width: Dimensions.get("window").width,
+        paddingHorizontal: 16,
+      }}
+    >
+      <Description style={styles.description}>
+        {format(date, "MMMM dd, yyyy")}
+      </Description>
+
+      {logs.length === 0 && (
+        <View style={styles.noActivityView}>
+          <Text style={styles.noActivityText}>No activity</Text>
+        </View>
+      )}
+
+      {logs.length > 0 && (
+        <ScrollView contentContainerStyle={styles.scrollView}>
+          {logs.map((log) => (
+            <ActivityLogItem key={log.id} log={log} />
+          ))}
+        </ScrollView>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  description: {
+    marginBottom: 20,
+  },
+  noActivityView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  noActivityText: {
+    fontSize: 14,
+    color: getColor("mutedForeground"),
+    marginTop: -50,
+  },
+  scrollView: {
+    flex: 1,
+    gap: 16,
+  },
+});
