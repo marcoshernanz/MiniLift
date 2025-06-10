38ad0019f66091fe3e450dd65dfe470928a9615b
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-06-10 16:38:53 +0200

    activity edit modal

--- components/activity/ActivityEditModal.tsx
+++ components/activity/ActivityEditModal.tsx
@@ -1,5 +1,11 @@
+import getColor from "@/lib/getColor";
 import { LogType } from "@/lib/hooks/useActivity";
-import { Modal } from "react-native";
+import { XIcon } from "lucide-react-native";
+import { Dimensions, Modal, StyleSheet, View } from "react-native";
+import LogBodyweight from "../log/LogBodyweight";
+import LogLift from "../log/LogLift";
+import Button from "../ui/Button";
+import SafeArea from "../ui/SafeArea";
 
 interface Props {
   log: LogType;
@@ -8,5 +14,63 @@ interface Props {
 }
 
 export default function ActivityEditModal({ log, visible, onClose }: Props) {
-  return <Modal visible={visible} onRequestClose={onClose}></Modal>;
+  return (
+    <Modal
+      statusBarTranslucent={true}
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}
+      animationType="slide"
+    >
+      <SafeArea style={styles.safeArea}>
+        <View style={styles.container}>
+          {log.kind === "lift" && <LogLift onClose={onClose} />}
+          {log.kind === "bodyweight" && <LogBodyweight onClose={onClose} />}
+
+          <Button
+            variant="ghost"
+            containerStyle={styles.closeButtonContainer}
+            pressableStyle={styles.closeButtonPressable}
+            onPress={onClose}
+          >
+            <XIcon color={getColor("foreground")} />
+          </Button>
+        </View>
+        <Button
+          variant="destructive"
+          containerStyle={styles.deleteButtonContainer}
+        >
+          Delete
+        </Button>
+      </SafeArea>
+    </Modal>
+  );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    justifyContent: "space-between",
+    height: Dimensions.get("screen").height,
+    paddingHorizontal: 0,
+    paddingTop: 0,
+    flex: 0,
+  },
+  container: {
+    position: "relative",
+    flex: 1,
+  },
+  closeButtonContainer: {
+    position: "absolute",
+    top: 20,
+    right: 16,
+    borderRadius: 9999,
+    zIndex: 10,
+  },
+  closeButtonPressable: {
+    borderRadius: 9999,
+    padding: 10,
+    height: 38,
+    width: 38,
+  },
+  deleteButtonContainer: { paddingHorizontal: 16, paddingVertical: 20 },
+});
