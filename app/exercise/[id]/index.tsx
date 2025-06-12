c4c0a41382e05faf0c776d892209d51421b5ca2a
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-06-12 18:16:27 +0200

    exercise details header

--- app/exercise/[id]/index.tsx
+++ app/exercise/[id]/index.tsx
@@ -1,77 +1,29 @@
-import Button from "@/components/ui/Button";
+import ExerciseDetailsHeader from "@/components/exercises/details/ExerciseDetailsHeader";
 import SafeArea from "@/components/ui/SafeArea";
-import Title from "@/components/ui/Title";
 import { useAppContext } from "@/context/AppContext";
-import getColor from "@/lib/getColor";
 import { useLocalSearchParams } from "expo-router";
-import { StarIcon } from "lucide-react-native";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 export default function ExerciseDetailScreen() {
   const { id } = useLocalSearchParams<{ id: string }>();
-  const { appData, setAppData } = useAppContext();
+  const { appData } = useAppContext();
+
   const exercise = appData.exercises[id];
 
   if (!exercise) return null;
 
-  const logs = appData.liftLogs
-    .filter((log) => log.exercise.id === id)
-    .sort((a, b) => b.date.getTime() - a.date.getTime());
+  // const logs = appData.liftLogs
+  //   .filter((log) => log.exercise.id === id)
+  //   .sort((a, b) => b.date.getTime() - a.date.getTime());
 
-  const mostRecent = logs[0];
-
-  const toggleFavorite = () => {
-    setAppData((prev) => {
-      const exercise = prev.exercises[id];
-      const updatedExercise = { ...exercise, isFavorite: !exercise.isFavorite };
-      return {
-        ...prev,
-        exercises: { ...prev.exercises, [id]: updatedExercise },
-      };
-    });
-  };
+  // const mostRecent = logs[0];
 
   return (
-    <SafeArea edges={["top", "left", "right"]}>
-      <View style={styles.container}>
-        <Title>{exercise.name}</Title>
-        <Button
-          variant="ghost"
-          containerStyle={styles.buttonContainer}
-          pressableStyle={styles.buttonPressable}
-          onPress={toggleFavorite}
-        >
-          <StarIcon
-            color={
-              exercise.isFavorite
-                ? getColor("primary")
-                : getColor("mutedForeground")
-            }
-            fill={exercise.isFavorite ? getColor("primary") : "transparent"}
-          />
-        </Button>
-      </View>
+    <SafeArea>
+      <ExerciseDetailsHeader exercise={exercise} />
     </SafeArea>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    position: "relative",
-  },
-  buttonContainer: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    borderRadius: 9999,
-    zIndex: 10,
-  },
-  buttonPressable: {
-    borderRadius: 9999,
-    padding: 10,
-    height: 42,
-    width: 42,
-  },
-});
+const styles = StyleSheet.create({});
---
+++ components/exercises/details/ExerciseDetailsHeader.tsx
@@ -0,0 +1,70 @@
+import Button from "@/components/ui/Button";
+import Title from "@/components/ui/Title";
+import { useAppContext } from "@/context/AppContext";
+import getColor from "@/lib/getColor";
+import { Exercise } from "@/zod/schemas/ExerciseSchema";
+import { StarIcon } from "lucide-react-native";
+import { StyleSheet, View } from "react-native";
+
+interface Props {
+  exercise: Exercise;
+}
+
+export default function ExerciseDetailsHeader({ exercise }: Props) {
+  const { setAppData } = useAppContext();
+
+  const toggleFavorite = () => {
+    setAppData((prev) => {
+      const currExercise = prev.exercises[exercise.id];
+      const updatedExercise = {
+        ...currExercise,
+        isFavorite: !currExercise.isFavorite,
+      };
+      return {
+        ...prev,
+        exercises: { ...prev.exercises, [exercise.id]: updatedExercise },
+      };
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Title>{exercise.name}</Title>
+      <Button
+        variant="ghost"
+        containerStyle={styles.buttonContainer}
+        pressableStyle={styles.buttonPressable}
+        onPress={toggleFavorite}
+      >
+        <StarIcon
+          color={
+            exercise.isFavorite
+              ? getColor("primary")
+              : getColor("mutedForeground")
+          }
+          fill={exercise.isFavorite ? getColor("primary") : "transparent"}
+        />
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "relative",
+  },
+  buttonContainer: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    borderRadius: 9999,
+    zIndex: 10,
+  },
+  buttonPressable: {
+    borderRadius: 9999,
+    padding: 10,
+    height: 42,
+    width: 42,
+  },
+});
