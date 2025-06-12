5544ec4ce5dff69859254a3e14f9ff4d85e3bf81
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-06-12 17:00:32 +0200

    different data for the different enviroments

--- config/appConfig.ts
+++ config/appConfig.ts
@@ -1,3 +1,15 @@
+import Constants from "expo-constants";
+
+const appVariant = Constants.expoConfig?.extra?.APP_VARIANT;
+const storageKeyBase = "appState";
+
+const storageKey =
+  appVariant === "development"
+    ? `${storageKeyBase}-dev`
+    : appVariant === "preview"
+    ? `${storageKeyBase}-preview`
+    : storageKeyBase;
+
 export const appConfig = {
-  storageKey: "appState",
+  storageKey,
 } as const;
