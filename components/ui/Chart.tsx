abfdccb61a91f37c3f83aeca8ef0a42892e1d1b0
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-06-26 17:49:49 +0200

    animate only visible chart parts

--- components/ui/Chart.tsx
+++ components/ui/Chart.tsx
@@ -246,6 +246,10 @@ export default function Chart({
     ],
   });
 
+  const chartStart = useDerivedValue(() =>
+    animationProgress.value === 1 ? 0 : 1 - (width * 2) / chartWidth
+  );
+
   return (
     <GestureDetector gesture={gesture}>
       <View style={{ width, height, flexDirection: "column" }}>
@@ -270,6 +274,7 @@ export default function Chart({
                 color={getColor("primary")}
                 style="stroke"
                 strokeWidth={2}
+                start={chartStart}
               />
 
               {points.map((p, idx) => {
