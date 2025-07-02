2aa85df32814a227574a9500492ac1f4f7be7213
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-07-02 18:06:01 +0200

    home scaffolding

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,26 @@
+import HomeActivity from "@/components/home/HomeActivity";
+import HomeBodyweight from "@/components/home/HomeBodyweight";
+import HomeScore from "@/components/home/HomeScore";
 import SafeArea from "@/components/ui/SafeArea";
+import Title from "@/components/ui/Title";
+import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
-  return <SafeArea></SafeArea>;
+  return (
+    <SafeArea style={{ paddingHorizontal: 0 }} edges={["top", "left", "right"]}>
+      <View style={styles.container}>
+        <Title style={{ paddingBottom: 20 }}>Home</Title>
+        <HomeScore />
+        <HomeBodyweight />
+        <HomeActivity />
+      </View>
+    </SafeArea>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+});
