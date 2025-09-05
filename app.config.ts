8f256f9df66845f545a3f50a96f1b4df55a4d6a4
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-05 14:02:43 +0200

    bump up version

--- app.config.ts
+++ app.config.ts
@@ -28,7 +28,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: getAppName(),
   slug: "MiniLift",
-  version: "1.0.5",
+  version: "1.0.6",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "minilift",
